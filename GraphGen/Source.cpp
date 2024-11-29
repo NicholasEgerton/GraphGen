79f8c6cbbf8a15394f757cfcf8a6043a970a14c3
Nicholas Egerton <egertonnick16@gmail.com>
2024-11-29 21:55:35 +1100

    Cleaning up zoom

--- GraphGen/Source.cpp
+++ GraphGen/Source.cpp
@@ -3,75 +3,13 @@
 //Work of Nicholas Egerton
 
 #include "SFML/Graphics.hpp"
-#include "Parameters.h"
 #include <iostream>
 #include <iomanip>
+#include "Parameters.h"
 #include "Utils.h"
+#include "ZoomHandle.h"
 
 using namespace sf;
-
-void ZoomScaleTexts(float delta, int precision, Text* horizontalScaleTexts, Text* verticalScaleTexts, RenderWindow* window) {
-
-    float factor = 0;
-    if (delta >= 1) {
-        factor = 0.5;
-    }
-
-    else {
-        factor = 2;
-    }
-
-    for (int i = 0; i < 8; i++) {
-        std::string s = horizontalScaleTexts[i].getString();
-
-        float f = std::stof(s);
-
-        f *= factor;
-
-        s = Utils::FloatToString(f, precision);
-
-        horizontalScaleTexts[i].setString(s);
-
-        f *= -1;
-        s = Utils::FloatToString(f, precision);
-        verticalScaleTexts[i].setString(s);
-    }
-}
-
-
-void Zoom(float& precision, float& delta, Parameters* parameters, Text* horizontalScaleTexts, Text* verticalScaleTexts, RenderWindow* window) {
-
-    if (parameters->GetScale() >= 1) {
-        precision = 0;
-    }
-
-    //MIN LOWER BOUND OF 1.90735e-06
-    if (parameters->GetScale() > 1.90735e-06) {
-        if (delta >= 1) {
-            parameters->SetScale(parameters->GetScale() / 2);
-            precision += 0.34f;
-
-            ZoomScaleTexts(delta, static_cast<int>(roundf(precision)), horizontalScaleTexts, verticalScaleTexts, window);
-            //The final argument true says to ignore the values
-            //Being the same as we are zooming
-            parameters->Generate(Vector2i(window->getSize()), true);
-        }
-    }
-
-    //MAX UPPPER BOUND OF 131072
-    if (parameters->GetScale() < 131072) {
-        if (delta <= -1) {
-            parameters->SetScale(parameters->GetScale() * 2);
-            precision -= 0.34f;
-
-            ZoomScaleTexts(delta, static_cast<int>(roundf(precision)), horizontalScaleTexts, verticalScaleTexts, window);
-
-            //The final argument true says to ignore the values
-            //Being the same as we are zooming
-            parameters->Generate(Vector2i(window->getSize()), true);
-        }
-    }
-}
 
 int main()
 {
@@ -210,13 +148,13 @@ int main()
             }
 
             if (event.type == Event::MouseWheelScrolled) {
-                Zoom(precision, event.mouseWheelScroll.delta, &parameters, horizontalScaleTexts, verticalScaleTexts, &window);
+                ZoomHandle::Zoom(precision, event.mouseWheelScroll.delta, &parameters, horizontalScaleTexts, verticalScaleTexts, &window);
             }
         }
 
         //Key zoom at approximately 30fps
         if (keyZoom.zooming && zoomClock.getElapsedTime().asSeconds() >= 0.033f) {
-            Zoom(precision, keyZoom.key, &parameters, horizontalScaleTexts, verticalScaleTexts, &window);
+            ZoomHandle::Zoom(precision, keyZoom.key, &parameters, horizontalScaleTexts, verticalScaleTexts, &window);
             zoomClock.restart();
         }
 
